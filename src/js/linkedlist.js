8a5ba2ea9b0435e1d7e451dfa179af9447dc8c88
dhavalsinhzala <dhaval.zala@live.com>
2015-08-03 12:13:48 +0530

    indention changes

--- src/js/linkedlist.js
+++ src/js/linkedlist.js
@@ -1,53 +1,53 @@
 function linkedList() {
-	var list = {}, start = null;
+    var list = {}, start = null;
 
-	function makeNode(data) {
-		return {
-			data : data,
-			count : 1,
-			next : null
-		};
-	}
+    function makeNode(data) {
+        return {
+            data : data,
+            count : 1,
+            next : null
+        };
+    }
 
-	list.add = function(data) {
-		var node = makeNode(data);
-		if (start === null) {
-			start = node;
-		} else {
-			var save = start, prev = null;
-			while (save != null && save.data < data) {
-				prev = save;
-				save = save.next;
-			}
+    list.add = function(data) {
+        var node = makeNode(data);
+        if (start === null) {
+            start = node;
+        } else {
+            var save = start, prev = null;
+            while (save != null && save.data < data) {
+                prev = save;
+                save = save.next;
+            }
 
-			if (save != null && save.data === data) {
-				save.count++;
-				return;
-			} else {
-				node.next = save;
-				if (save === start) {
-					start = node;
-				} else {
-					prev.next = node;
-				}
-			}
-		}
-	};
+            if (save != null && save.data === data) {
+                save.count++;
+                return;
+            } else {
+                node.next = save;
+                if (save === start) {
+                    start = node;
+                } else {
+                    prev.next = node;
+                }
+            }
+        }
+    };
 
-	list.occurencesList = function() {
-		var save = start, results = [];
-		while (save != null) {
-			results.push({
-				data : save.data,
-				count : save.count
-			});
-			save = save.next;
-		}
-		return results;
-	};
+    list.occurencesList = function() {
+        var save = start, results = [];
+        while (save != null) {
+            results.push({
+                data : save.data,
+                count : save.count
+            });
+            save = save.next;
+        }
+        return results;
+    };
 
-	list.remove = function() {
+    list.remove = function() {
 
-	};
-	return list;
+    };
+    return list;
 }
--- src/js/word-count.js
+++ src/js/word-count.js
@@ -1,18 +1,18 @@
 (function() {
-	var app = angular.module("word-count", []);
-	app.controller("IndexController", [ '$scope', function($scope) {
-		$scope.list = [];
+    var app = angular.module("word-count", []);
+    app.controller("IndexController", [ '$scope', function($scope) {
+        $scope.list = [];
 
-		$scope.find = function(inputText) {
-			var sList = new linkedList(), dataItems = inputText.split(" ");
+        $scope.find = function(inputText) {
+            var sList = new linkedList(), dataItems = inputText.split(" ");
 
-			for (var index = 0; index < dataItems.length; index++) {
-				if (dataItems[index] !== "") {
-					sList.add(dataItems[index]);
-				}
-			}
+            for (var index = 0; index < dataItems.length; index++) {
+                if (dataItems[index] !== "") {
+                    sList.add(dataItems[index]);
+                }
+            }
 
-			this.list = sList.occurencesList();
-		};
-	} ]);
+            this.list = sList.occurencesList();
+        };
+    } ]);
 })();
