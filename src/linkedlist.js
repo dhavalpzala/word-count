7e6c98746dd6aa05487038e3870d48c33962d759
dhavalsinhzala <dhaval.zala@live.com>
2015-08-05 15:07:05 +0530

    resolved scope issue

--- src/linkedlist.js
+++ src/linkedlist.js
@@ -1,5 +1,44 @@
-function linkedList() {
-    var list = {}, start = null;
+var linkedList = (function() {
+    var list = function() {
+        var start = null;
+
+        this.add = function(data) {
+            var node = makeNode(data);
+            if (start === null) {
+                start = node;
+            } else {
+                var save = start, prev = null;
+                while (save != null && save.data < data) {
+                    prev = save;
+                    save = save.next;
+                }
+
+                if (save != null && save.data === data) {
+                    save.count++;
+                    return;
+                } else {
+                    node.next = save;
+                    if (save === start) {
+                        start = node;
+                    } else {
+                        prev.next = node;
+                    }
+                }
+            }
+        };
+
+        this.occurencesList = function() {
+            var save = start, results = [];
+            while (save != null) {
+                results.push({
+                    data : save.data,
+                    count : save.count
+                });
+                save = save.next;
+            }
+            return results;
+        };
+    };
 
     function makeNode(data) {
         return {
@@ -9,45 +48,5 @@ function linkedList() {
         };
     }
 
-    list.add = function(data) {
-        var node = makeNode(data);
-        if (start === null) {
-            start = node;
-        } else {
-            var save = start, prev = null;
-            while (save != null && save.data < data) {
-                prev = save;
-                save = save.next;
-            }
-
-            if (save != null && save.data === data) {
-                save.count++;
-                return;
-            } else {
-                node.next = save;
-                if (save === start) {
-                    start = node;
-                } else {
-                    prev.next = node;
-                }
-            }
-        }
-    };
-
-    list.occurencesList = function() {
-        var save = start, results = [];
-        while (save != null) {
-            results.push({
-                data : save.data,
-                count : save.count
-            });
-            save = save.next;
-        }
-        return results;
-    };
-
-    list.remove = function() {
-
-    };
     return list;
-}
+})();
